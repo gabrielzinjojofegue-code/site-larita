3b329a6532ebba1d60c9cd49e32c30caa87feb47
gabrielzinjojofegue-code <gabrielzinjojofegue@gmail.com>
2026-04-16 21:03:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,4 +10,38 @@ envelope.addEventListener("click", () => {
   } else {
     musica.pause();
   }
+
+  const heartsContainer = document.querySelector(".hearts");
+
+function createHeart() {
+  const heart = document.createElement("div");
+  heart.classList.add("heart");
+
+  // coração emoji
+  heart.innerText = "💖";
+
+  // posição aleatória
+  heart.style.left = Math.random() * 100 + "vw";
+
+  // tamanho aleatório
+  heart.style.fontSize = (Math.random() * 20 + 10) + "px";
+
+  // duração aleatória
+  heart.style.animationDuration = (Math.random() * 3 + 2) + "s";
+
+  // cores rosa e azul
+  const colors = ["#ff4d6d", "#4da6ff"];
+  heart.style.color = colors[Math.floor(Math.random() * colors.length)];
+
+  heartsContainer.appendChild(heart);
+
+  // remove depois de cair
+  setTimeout(() => {
+    heart.remove();
+  }, 5000);
+}
+
+// cria vários corações continuamente
+setInterval(createHeart, 300);
+
 });
